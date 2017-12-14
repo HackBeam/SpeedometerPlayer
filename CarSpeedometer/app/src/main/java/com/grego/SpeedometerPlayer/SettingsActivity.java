cb7c2a168b98ad3d39a26b1f8600f456bb42222a
Gregorio Carvajal Exposio <gregoaplweb@gmail.com>
2017-12-14 12:44:45 +0100

    Brightness on settings page
    * En la pantalla de ajustes se mantiene el brillo al 100%

--- CarSpeedometer/app/src/main/java/com/grego/SpeedometerPlayer/SettingsActivity.java
+++ CarSpeedometer/app/src/main/java/com/grego/SpeedometerPlayer/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.grego.SpeedometerPlayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.grego.SpeedometerPlayer.R;
 
@@ -12,6 +13,11 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        //BRILLO 100%
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = 1.0f;
+        getWindow().setAttributes(lp);
+        
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
