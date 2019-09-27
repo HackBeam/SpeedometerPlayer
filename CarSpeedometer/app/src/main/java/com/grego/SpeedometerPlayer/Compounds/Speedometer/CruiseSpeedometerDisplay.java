7bdb0be2e3a21d3a24043625639be749f59a7655
Grego <grego@besoccergames.com>
2019-09-27 13:14:49 +0200

    Implement cruise arrow indicator

--- CarSpeedometer/app/src/main/java/com/grego/SpeedometerPlayer/Compounds/Speedometer/CruiseSpeedometerDisplay.java
+++ CarSpeedometer/app/src/main/java/com/grego/SpeedometerPlayer/Compounds/Speedometer/CruiseSpeedometerDisplay.java
@@ -3,6 +3,7 @@ package com.grego.SpeedometerPlayer.Compounds.Speedometer;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.grego.SpeedometerPlayer.Core;
@@ -14,12 +15,26 @@ import com.grego.SpeedometerPlayer.Services.Listeners.IInputListener;
  */
 public class CruiseSpeedometerDisplay extends ConstraintLayout implements IInputListener
 {
+    private static final int DESIRED_SPEED_OFFSET = 5;
+    private static final int BACKGROUND_DEACTIVATED_ALPHA = 128;
+
     private SpeedometerDisplay speedometer;
     private TextView desiredSpeedText;
+    private ImageView backgroundBelow;
+    private ImageView backgroundGood;
+    private ImageView backgroundAbove;
+    private ImageView arrowIndicator;
 
-    private static final int DESIRED_SPEED_OFFSET = 5;
     private int desiredSpeed = 120;
     private boolean active = false;
+
+    //region Rotation algorithm cache
+    private int minSpeedIndicator;
+    private float currentArrowDegrees = 0;
+    private static final float INDICATOR_SPEED_RANGE = DESIRED_SPEED_OFFSET * 6;
+    private static final int ROTATION_OFFSET = 90;
+    private static final int ROTATION_RANGE = ROTATION_OFFSET * 2;
+    //endregion
 
     public CruiseSpeedometerDisplay(Context context)
     {
@@ -48,11 +63,19 @@ public class CruiseSpeedometerDisplay extends ConstraintLayout implements IInput
             // Get UI object references
             speedometer = (SpeedometerDisplay) findViewById(R.id.cruise_speedometer_display);
             desiredSpeedText = (TextView) findViewById(R.id.cruise_desired_value);
+            backgroundAbove = (ImageView) findViewById(R.id.cruise_above_background);
+            backgroundGood = (ImageView) findViewById(R.id.cruise_desired_background);
+            backgroundBelow = (ImageView) findViewById(R.id.cruise_below_background);
+            arrowIndicator = (ImageView) findViewById(R.id.cruise_arrow_indicator);
 
             // Set default preferences
             this.setAlpha(0);
             desiredSpeedText.setTypeface(Core.Data.DefaultFont);
             Core.Helpers.SetColorToTextView(desiredSpeedText, Core.Data.Colors.cruiseDesired);
+            backgroundAbove.setColorFilter(Core.Data.Colors.cruiseAbove);
+            backgroundGood.setColorFilter(Core.Data.Colors.cruiseGood);
+            backgroundBelow.setColorFilter(Core.Data.Colors.cruiseBelow);
+
             SetColor(Core.Data.Colors.normal);
         }
     }
@@ -68,23 +91,40 @@ public class CruiseSpeedometerDisplay extends ConstraintLayout implements IInput
         if (speedometer.GetSpeedValue() >= desiredSpeed + DESIRED_SPEED_OFFSET) // Above good speed
         {
             SetColor(Core.Data.Colors.cruiseAbove);
+            backgroundAbove.setImageAlpha(255);
+            backgroundGood.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
+            backgroundBelow.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
         }
         else if (speedometer.GetSpeedValue() <= desiredSpeed - DESIRED_SPEED_OFFSET) // Below good speed
         {
             SetColor(Core.Data.Colors.cruiseBelow);
+            backgroundAbove.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
+            backgroundGood.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
+            backgroundBelow.setImageAlpha(255);
         }
         else // In good desired speed
         {
             SetColor(Core.Data.Colors.cruiseGood);
+            backgroundAbove.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
+            backgroundGood.setImageAlpha(255);
+            backgroundBelow.setImageAlpha(BACKGROUND_DEACTIVATED_ALPHA);
         }
+
+        UpdateArrow();
+    }
+
+    private void UpdateArrow()
+    {
+        float normalized = (speedometer.GetSpeedValue() - minSpeedIndicator) / INDICATOR_SPEED_RANGE;
+        float rotation = normalized * ROTATION_RANGE - ROTATION_OFFSET;
+        arrowIndicator.setRotation(rotation);
     }
 
     //region IInputListener events
     @Override
     public void OnSingleTap()
     {
-        desiredSpeed = speedometer.GetSpeedValue();
-        UpdateUI();
+        SetDesiredSpeed();
     }
 
     //region Unused Input Events
@@ -128,7 +168,19 @@ public class CruiseSpeedometerDisplay extends ConstraintLayout implements IInput
     //endregion
 
     /**
+     * Sets the desired speed to the current speed
+     * and updates the rotation algorithm cache vars.
+     */
+    private void SetDesiredSpeed()
+    {
+        desiredSpeed = speedometer.GetSpeedValue();
+        minSpeedIndicator = desiredSpeed - DESIRED_SPEED_OFFSET - DESIRED_SPEED_OFFSET - DESIRED_SPEED_OFFSET;
+        UpdateUI();
+    }
+
+    /**
      * Enables or disables the compound.
+     *
      * @param active Whether or not the compound should be visible and active.
      */
     public void SetActive(boolean active)
@@ -139,7 +191,7 @@ public class CruiseSpeedometerDisplay extends ConstraintLayout implements IInput
             Core.Services.Input.Subscribe(this);
             this.setAlpha(1);
 
-            OnSingleTap();
+            SetDesiredSpeed();
         }
         else if (!active && this.active)
         {
